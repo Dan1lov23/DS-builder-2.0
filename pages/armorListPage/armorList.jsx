f06de86043a703ac53e14bf6a77c72ce8710b5ae
kitik <svy1dan@gmail.com>
2025-03-25 13:04:06 +0300

    Add armour list directory with armour panel

---
+++ pages/armorListPage/armorList.jsx
@@ -0,0 +1,25 @@
+import './armorList.css'
+
+export default function ArmorList() {
+    return (
+        <>
+            <div className="armourListMain">
+                <div className="armorListMainContanier">
+                    <div className="armor">
+                        <img src="https://t3.ftcdn.net/jpg/02/61/04/02/360_F_261040216_dI5MBh32t9TXeLN1l3mSmNCwILuNcUi5.jpg"/>
+                    </div>
+                    <div className="armor">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-PqKTzgKGtXkz7PvKWDg6JYjzJCxLchxqbfT-GQnFxh7a6WuMX0dPik7-2hn0q6DncA&usqp=CAU"/>
+                    </div>
+                    <div className="armor">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhb12ZrK6GdnwWvbNv423BFOBSjsoE-9KRdqT4Xl91gEXNDhAInRltN96TLytTcZpNHu4&usqp=CAU"/>
+                    </div>
+                    <div className="armor">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwBHY2cqc_l0FhFQvhZ9oCZRixXi6pVHCEUg&s"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
