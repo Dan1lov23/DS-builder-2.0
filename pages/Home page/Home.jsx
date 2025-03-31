98ab01d66169f7d6fa39e68d842d4ef9dd9c724b
kitik <svy1dan@gmail.com>
2025-03-31 09:05:17 +0300

    And minor changes on JSX code.

--- pages/Home page/Home.jsx
+++ pages/Home page/Home.jsx
@@ -3,6 +3,7 @@ import EquipAdd from '../../components/equip add component/EquipAdd.jsx';
 import EquipList from "../equip list page/equipList.jsx";
 import { useState, useEffect } from "react";
 import ArmorList from "../armorListPage/armorList.jsx";
+import AddBosess from "../../components/add boss component/AddBoss.jsx";
 
 export default function Home() {
     const [userItem, setUserItem] = useState([]); // Инициализация состояния userItem как пустого массива
@@ -105,6 +106,7 @@ export default function Home() {
                 removeItem={removeItem}
                 requirementsItem={requirementsItem}
             />
+            <AddBosess/>
             <EquipList
                 userItem={userItem}
                 setUserItem={setUserItem}
