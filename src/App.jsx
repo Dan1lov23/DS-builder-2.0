513541187548a5a6be4fdd8c1ed19499a6384bf0
kitik <svy1dan@gmail.com>
2025-04-04 21:43:35 +0300

    Minor update in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/Home page/Home.jsx'
-import EquipList from '../pages/equip list page/equipList.jsx'
-import EquipAdd from "../components/equip add component/EquipAdd.jsx";
-
-// импорты старнц брони
-
 import "../pages/addHelm/addHelm"
-import AddHelm from "../pages/addHelm/AddHelm.jsx";
 
 function App() {
     return (
@@ -15,8 +9,6 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />}/>
-                    <Route path="/equipList" element={<EquipList />}/>
-                    <Route path="/helms" element={<AddHelm />}/>
                 </Routes>
             </BrowserRouter>
         </>
