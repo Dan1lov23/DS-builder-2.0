e665c8cf12ce2295b8c91268bbdc011c3b35ad93
kitik <svy1dan@gmail.com>
2025-04-09 09:26:52 +0300

    Main coment in this project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/Home page/Home.jsx'
 import "../pages/addHelm/addHelm"
+import {StrictMode} from "react";
+
+//  <Route path="*" element={<Home />}/> я до этого прописывал дефолтный путь как "/", а надо было "*"
+// при прогрузке страницы ничего не отображалось я не мог понять что это
+// в общем впредь надо помнить, этот блок текста будет как напомнинание
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />}/>
+                    <Route path="*" element={<Home />}/>
                 </Routes>
             </BrowserRouter>
         </>
