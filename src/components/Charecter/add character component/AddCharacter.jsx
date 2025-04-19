678761ceda4398ff3b4b72d1ecb10b2647b4a669
kitik <svy1dan@gmail.com>
2025-04-19 12:26:29 +0300

    Change AddCharacter component

--- src/components/Charecter/add character component/AddCharacter.jsx
+++ src/components/Charecter/add character component/AddCharacter.jsx
@@ -1,18 +1,18 @@
+import './addCharacter.css';
+
 import Modal from '../../modal/Modal component/Modal.jsx';
 import CharacterList from "../characters list/Characters list.jsx";
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheck} from "@fortawesome/free-solid-svg-icons";
 import {useEffect, useState} from "react";
 
-export default function AddCharacter({chatacterClassName, setClassName, setClassSrc, setClassLevel, setClassAttutment, setClassVitality, setClassResist, setClassStrength, setClassDexterity, setClassIntelligence, setClassFaith}) {
-//
-    // хуки для резистов персов, прокидываю через компонент
+export default function AddCharacter({setClassName, setClassSrc, setClassLevel, setClassAttutment, setClassVitality, setClassResist, setClassStrength, setClassDexterity, setClassIntelligence, setClassFaith, setClassEndurance}) {
 
+    // хуки для резистов персов, прокидываю через компонент
     const [name, setName] = useState("");
     const [src, setSrc] = useState("");
     const [level, setLevel] = useState(0);
     const [vitality, setVitality] = useState(0);
+    const [endurance, setEndurance] = useState("");
     const [attunement, setAttunement] = useState(0);
     const [resist, setResistance] = useState(false);
     const [strength, setStrength] = useState(0);
@@ -21,15 +21,19 @@ export default function AddCharacter({chatacterClassName, setClassName, setClass
     const [faith, setFaith] = useState(0);
 
     useEffect(() => {
-        console.log(name, src, level, vitality, attunement, strength, dexterity, resist, intelligence, faith);
+        setClassName(name);
         setClassSrc(src);
         setClassLevel(level);
-        chatacterClassName = name;
-        setClassName(chatacterClassName);
-        console.log(setClassName, setClassLevel, setClassSrc);
+        setClassAttutment(attunement);
+        setClassVitality(vitality);
+        setClassResist(resist);
+        setClassEndurance(endurance);
+        setClassStrength(strength);
+        setClassDexterity(dexterity);
+        setClassFaith(faith);
+        setClassIntelligence(intelligence);
+        console.log(name, src, level, vitality, attunement, strength, dexterity, resist, intelligence, faith);
     }, [name])
-
-
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -39,11 +43,13 @@ export default function AddCharacter({chatacterClassName, setClassName, setClass
                 <CharacterList setName={setName} setSrc={setSrc} setLevel={setLevel}
                                setAttunement={setAttunement} setVitality={setVitality} setResistance={setResistance}
                                setStrength={setStrength} setDexterity={setDexterity}
-                               setIntelligence={setIntelligence} setFaith={setFaith} />
+                               setIntelligence={setIntelligence} setFaith={setFaith} setEndurance={setEndurance}/>
             </Modal>
             <div className="addCharacter">
                 <div className="addCharacterMainContanier" onClick={() => setIsModalOpen(true)}>
-                   <button><FontAwesomeIcon icon={faCheck} /></button>
+                   <button>
+                       <img src="https://t4.ftcdn.net/jpg/04/20/62/39/360_F_420623928_a5QOH1T7RWyHGTOjvZAoppF80fTiuvJK.jpg"/>
+                   </button>
                 </div>
             </div>
         </>
